69b13c0bf8771f511cd84b28d39db2e81891f417
anmansky <mannikola@gmail.com>
2022-04-28 13:41:53 +0300

    upd conf app-ssr-plus

--- SCRIPTS/02_prepare_package.sh
+++ SCRIPTS/02_prepare_package.sh
@@ -227,7 +227,7 @@ svn co https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus feeds/luci/ap
 ln -sf ../../../feeds/luci/applications/luci-app-ssr-plus ./package/feeds/luci/luci-app-ssr-plus
 ##### naiveproxy ssr plus
 #svn co https://github.com/immortalwrt/luci/trunk/applications/luci-app-ssr-plus feeds/luci/applications/luci-app-ssr-plus
-cp -rf ../PACK/luci/applications/luci-app-ssr-plus feeds/luci/applications/luci-app-ssr-plus
+#cp -rf ../PACK/luci/applications/luci-app-ssr-plus feeds/luci/applications/luci-app-ssr-plus
 # v2raya
 #git clone --depth 1 https://github.com/zxlhhyccc/luci-app-v2raya.git package/new/luci-app-v2raya
 #svn co https://github.com/v2rayA/v2raya-openwrt/trunk/v2raya package/new/v2raya
